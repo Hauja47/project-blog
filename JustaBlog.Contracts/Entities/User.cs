7675713b9cc19be8672c9406196053c2496b2177
Hauja47 <hoangletrunghau01@gmail.com>
2024-11-20 00:03:10 +0700

    Update User model

--- JustaBlog.Contracts/Entities/User.cs
+++ JustaBlog.Contracts/Entities/User.cs
@@ -3,8 +3,6 @@ namespace JustaBlog.Contracts.Entities;
 public class User : BaseEntity
 {
     public string Username { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Bio { get; set; }
